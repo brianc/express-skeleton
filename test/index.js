f4d21d4c0f46a51b5f5ca0f8fc72f224401c1839
brianc <brian.m.carlson@gmail.com>
2011-09-25 15:17:06 -0500

    make index test file

--- test/index.js
+++ test/index.js
@@ -1,13 +1,1 @@
-var tobi = require('tobi')
-  , drain = require('drain')
-  , app = require(__dirname + '/../')
-  , browser = tobi.createBrowser(app);
-
-browser.get('/', function(res, $) {
-  res.should.have.status(200);
-})
-
-browser.get('/index.css', function(res) {
-  res.should.have.status(200);
-  app.close();
-});
+require('test-dir')
